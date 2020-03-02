517b41dc6c577481672a6fc688d7210d0993703e
Lilit Ghazaryan <lilygh@rambler.ru>
2020-03-02 11:17:35 +0400

    added sql folder

---
+++ sql/libsystem.sql
@@ -0,0 +1,144 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: library
+-- ------------------------------------------------------
+-- Server version	5.7.21-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `authors`
+--
+
+DROP TABLE IF EXISTS `authors`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `authors` (
+  `auth_id` int(11) NOT NULL AUTO_INCREMENT,
+  `author` varchar(45) NOT NULL,
+  PRIMARY KEY (`auth_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `authors`
+--
+
+LOCK TABLES `authors` WRITE;
+/*!40000 ALTER TABLE `authors` DISABLE KEYS */;
+INSERT INTO `authors` VALUES (1,'Catherine o’Donovan'),(2,'Michael Keating'),(3,' Derek Mulvany'),(4,' James O Loughlin'),(5,'Lennart Reichenbach'),(6,'Sharon Meyler'),(7,'Niamh O Rourke'),(8,'James Hayes'),(9,'Roisin Mallon'),(10,'Declan Kennedy'),(11,'Rose Lawlor'),(12,'Sean Finn'),(13,'Thomas Koster');
+/*!40000 ALTER TABLE `authors` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `book_author`
+--
+
+DROP TABLE IF EXISTS `book_author`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `book_author` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `book_id` int(11) NOT NULL,
+  `auth_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `book_id_idx` (`book_id`),
+  KEY `auth_id_idx` (`auth_id`),
+  CONSTRAINT `auth_id` FOREIGN KEY (`auth_id`) REFERENCES `authors` (`auth_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `book_author_ibfk_1` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON DELETE NO ACTION ON UPDATE CASCADE,
+  CONSTRAINT `book_author_ibfk_2` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON UPDATE CASCADE,
+  CONSTRAINT `book_author_ibfk_3` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON DELETE CASCADE,
+  CONSTRAINT `book_author_ibfk_4` FOREIGN KEY (`auth_id`) REFERENCES `authors` (`auth_id`) ON DELETE CASCADE,
+  CONSTRAINT `book_author_ibfk_5` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON DELETE CASCADE,
+  CONSTRAINT `book_author_ibfk_6` FOREIGN KEY (`auth_id`) REFERENCES `authors` (`auth_id`) ON DELETE CASCADE,
+  CONSTRAINT `book_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `fk_id` FOREIGN KEY (`book_id`) REFERENCES `books` (`book_id`) ON DELETE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `book_author`
+--
+
+LOCK TABLES `book_author` WRITE;
+/*!40000 ALTER TABLE `book_author` DISABLE KEYS */;
+INSERT INTO `book_author` VALUES (1,1,1),(2,2,2),(3,2,3),(4,2,4),(5,3,2),(6,3,3),(7,3,4),(8,4,5),(9,5,6),(10,6,7),(11,7,8),(12,8,9),(13,9,9),(14,10,10),(15,10,11),(16,10,12),(17,11,13);
+/*!40000 ALTER TABLE `book_author` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `books`
+--
+
+DROP TABLE IF EXISTS `books`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `books` (
+  `book_id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(45) NOT NULL,
+  `isbn` varchar(13) NOT NULL,
+  PRIMARY KEY (`book_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `books`
+--
+
+LOCK TABLES `books` WRITE;
+/*!40000 ALTER TABLE `books` DISABLE KEYS */;
+INSERT INTO `books` VALUES (1,'Romeo & Juliet','9781910052878'),(2,'Active Maths 1','9781780900742'),(3,'Active Maths 2 ','9781780901312'),(4,'Allons En France 2','0717135144'),(5,'Expressions Francaises','9781847411754'),(6,'VielSpaB 2','9780714416690'),(7,'ZurSache 1','9780714418421'),(8,'Network','9781910468579'),(9,'Network Activities and Accounts Book','9781910468586'),(10,'Essential Science','9781780905693'),(11,'50 Artists You Should Know','9783791337166');
+/*!40000 ALTER TABLE `books` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Temporary view structure for view `lib`
+--
+
+DROP TABLE IF EXISTS `lib`;
+/*!50001 DROP VIEW IF EXISTS `lib`*/;
+SET @saved_cs_client     = @@character_set_client;
+SET character_set_client = utf8;
+/*!50001 CREATE VIEW `lib` AS SELECT 
+ 1 AS `title`,
+ 1 AS `isbn`,
+ 1 AS `author`*/;
+SET character_set_client = @saved_cs_client;
+
+--
+-- Final view structure for view `lib`
+--
+
+/*!50001 DROP VIEW IF EXISTS `lib`*/;
+/*!50001 SET @saved_cs_client          = @@character_set_client */;
+/*!50001 SET @saved_cs_results         = @@character_set_results */;
+/*!50001 SET @saved_col_connection     = @@collation_connection */;
+/*!50001 SET character_set_client      = utf8 */;
+/*!50001 SET character_set_results     = utf8 */;
+/*!50001 SET collation_connection      = utf8_general_ci */;
+/*!50001 CREATE ALGORITHM=UNDEFINED */
+/*!50013 DEFINER=`root`@`localhost` SQL SECURITY DEFINER */
+/*!50001 VIEW `lib` AS select `books`.`title` AS `title`,`books`.`isbn` AS `isbn`,`authors`.`author` AS `author` from ((`books` join `authors`) join `book_author`) where ((`book_author`.`book_id` = `books`.`book_id`) and (`book_author`.`auth_id` = `authors`.`auth_id`)) */;
+/*!50001 SET character_set_client      = @saved_cs_client */;
+/*!50001 SET character_set_results     = @saved_cs_results */;
+/*!50001 SET collation_connection      = @saved_col_connection */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-04-09  0:34:52
